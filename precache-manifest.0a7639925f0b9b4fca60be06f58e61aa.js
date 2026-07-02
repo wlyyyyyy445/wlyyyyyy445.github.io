71d66cb643f6bb6bdc6bdc80ae496176720be4ad
Website Developer <website@placeholder.local>
2026-07-02 19:21:11 +0800

    Updates

---
+++ precache-manifest.0a7639925f0b9b4fca60be06f58e61aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37331a4cd7e19995c133abf9660735ee",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b9f06dfae1d2a421e0b3",
+    "url": "/static/css/main.d98c5b5d.chunk.css"
+  },
+  {
+    "revision": "dddb3b395a33786674c2",
+    "url": "/static/js/2.ad40b8de.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.ad40b8de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9f06dfae1d2a421e0b3",
+    "url": "/static/js/main.fc877a8a.chunk.js"
+  },
+  {
+    "revision": "17cb4edd65ff0fc78083",
+    "url": "/static/js/runtime-main.9782fcb7.js"
+  }
+]);
